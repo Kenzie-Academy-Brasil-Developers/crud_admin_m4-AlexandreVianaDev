48e1b5b698ba65e4878a6d83341693c505f07003
AlexandreVianaDev <alexandreviana94@hotmail.com>
2023-04-17 08:59:41 -0300

    feat: handle errors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express, { Application, json } from 'express'
+import { errorHandler } from './middlewares/handle.middleware'
 
 const app: Application = express()
 app.use(json())
+
+app.post("/users", )
+
+// tem que por o .error ?
+app.use(errorHandler)
 
 export default app
